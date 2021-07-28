e6703f023458a49f2252d79c87ba83bd04d70fb2
adrian-padmodihardjo <adrian.pdmh@gmail.com>
2021-07-29 03:33:10 +0700

    create ButtonChipGroup component

---
+++ components/ButtonChipGroup/ButtonChipGroup.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="button-chip-group">
+    <slot />
+  </div>
+</template>
+
+<script>
+import { chipGroup } from './chip-group.symbol'
+export default {
+  provide () {
+    return {
+      [chipGroup]: this,
+    }
+  },
+  methods: {
+    setActive (child) {
+      if (Array.isArray(this.$children)) {
+        this.$children.forEach((c) => {
+          c.mActive = c === child
+        })
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@use "./ButtonChipGroup.scss";
+</style>
