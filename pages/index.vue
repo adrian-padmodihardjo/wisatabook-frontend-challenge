a4bc1d00948af322b3e5c0044426b788c2632aec
adrian-padmodihardjo <adrian.pdmh@gmail.com>
2021-07-29 03:33:56 +0700

    insert property gallery image tags on homepage

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,10 @@
   <div>
     <TheHeader />
     <ResponsiveContainer>
+      <PropertyGalleryTags
+        property-id="9000248525"
+        style="margin: 0 -12px; padding: 16px 0;"
+      />
       <PropertyGallery property-id="9000248525" />
     </ResponsiveContainer>
   </div>
@@ -11,11 +15,13 @@
 import { TheHeader } from '@/components/Layouts/TheHeader'
 import { ResponsiveContainer } from '@/components/Layouts/ResponsiveContainer'
 import { PropertyGallery } from '@/components/PropertyGallery'
+import { PropertyGalleryTags } from '@/components/PropertyGalleryTags'
 export default {
   components: {
     TheHeader,
     ResponsiveContainer,
     PropertyGallery,
+    PropertyGalleryTags,
   },
 }
 </script>
