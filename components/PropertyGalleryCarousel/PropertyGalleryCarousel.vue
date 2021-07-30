35a9e2056c68054a5b29295858a13bd522b028be
adrian-padmodihardjo <adrian.pdmh@gmail.com>
2021-07-31 04:24:39 +0700

    create PropertyGalleryCarousel component

---
+++ components/PropertyGalleryCarousel/PropertyGalleryCarousel.vue
@@ -0,0 +1,29 @@
+<template>
+  <ImageCarousel
+    width="900px"
+    max-width="85vw"
+    v-bind="$props"
+    v-on="$listeners"
+  >
+    <template #item="{ item }">
+      <LazyImage
+        class="prop-gallery-carousel__img"
+        :alt="item.caption"
+        :srcset="item.srcset"
+      />
+    </template>
+  </ImageCarousel>
+</template>
+
+<script>
+import { ImageCarousel } from '@/components/ImageCarousel'
+import LazyImage from '../LazyImage/LazyImage.vue'
+
+export default {
+  components: {
+    ImageCarousel,
+    LazyImage,
+  },
+  extends: ImageCarousel,
+}
+</script>
---
+++ components/PropertyGalleryCarousel/index.js
@@ -0,0 +1,3 @@
+import PropertyGalleryCarousel from './PropertyGalleryCarousel'
+export { PropertyGalleryCarousel }
+export default PropertyGalleryCarousel
