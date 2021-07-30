09aafc59ddadbb6db1edf699f6b1a60013b210a7
adrian-padmodihardjo <adrian.pdmh@gmail.com>
2021-07-30 21:56:52 +0700

    render image by these priority: 1) image has been loaded at least one 2) if 'onVisibleOnly' is true, render if image visible within viewport 3) image minWidth rule matched with current viewport width

--- components/LazyImage/LazyImage.vue
+++ components/LazyImage/LazyImage.vue
@@ -3,6 +3,7 @@
     <template v-for="set in srcset">
       <img
         v-if="canRenderImage(set)"
+        ref="imgElements"
         :key="set.src"
         :src="set.src"
         class="lazy-img__image"
@@ -16,7 +17,8 @@
 /**
  * @typedef {object} ImageSet
  * @property {string} src - image src
- * @property {number} minWidth - image will be shown on min-width
+ * @property {number} minWidth - image will be rendered on min-width
+ * @property {boolean} onVisibleOnly - image will be rendered only when visible within viewport
  */
 export default {
   props: {
@@ -35,9 +37,45 @@ export default {
   data () {
     return {
       loadedImages: [],
+      intersectionObserver: null,
+      isVisible: false,
     }
   },
+  async mounted () {
+    await this.$nextTick()
+    this.watchSrcSet()
+  },
   methods: {
+    watchSrcSet () {
+      this.$watch(
+        function () {
+          return this.srcset
+        },
+        function () {
+          this.destroyObserver()
+          this.initObserver()
+        },
+        { immediate: true, deep: true },
+      )
+    },
+    observerCallback (entries) {
+      const entry = entries[0]
+      this.isVisible = entry.isIntersecting
+    },
+    initObserver () {
+      this.intersectionObserver = new IntersectionObserver(this.observerCallback, {
+        root: null,
+        rootMargin: '0px 0px 0px 0px',
+        threshold: 0.5,
+      })
+      this.intersectionObserver.observe(this.$el)
+    },
+    destroyObserver () {
+      if (this.intersectionObserver instanceof IntersectionObserver) {
+        this.intersectionObserver.disconnect()
+        this.intersectionObserver = null
+      }
+    },
     isImageLoaded (imageSet) {
       return this.loadedImages.includes(imageSet.src)
     },
@@ -48,8 +86,14 @@ export default {
       this.loadedImages.push(imageSet.src)
     },
     canRenderImage (imageSet) {
-      return this.isImageLoaded(imageSet)
-        || this.isImageMinWidthMatched(imageSet)
+      if (this.isImageLoaded(imageSet)) {
+        return true
+      }
+      if (imageSet.onVisibleOnly) {
+        return this.isVisible
+          && this.isImageMinWidthMatched(imageSet)
+      }
+      return this.isImageMinWidthMatched(imageSet)
     },
   },
 }
